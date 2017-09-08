9e154e7d587f42fb90bf12bd03dead180590b0c2
GOVERNET\reubenellis <rellis@governet.net>
2017-09-08 11:31:48 -0600

    initial commit of C++ Tutorials

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+//main() is where program execution begins.
+int main() {
+    cout << "This is my first C++ program." << endl;
+
+    cout << dataTypes();
+    return 0;
+}
